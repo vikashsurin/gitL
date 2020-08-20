23822cfeac728bb5f9386d481c970641bd62d71f
vikash surin <vikashsurin10@gmail.com>
2020-08-20 20:06:27 +0530

    adding hello

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <h1>Hello i am Los</h1>
       <h2>this is a new update.</h2>
+      <h2>I am here again ,haha..</h2>
+      <h2>hello there</h2>
     </div>
   );
 }
