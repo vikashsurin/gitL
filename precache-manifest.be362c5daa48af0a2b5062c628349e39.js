a84b62a0ba64f37aa9a7a0eb3826fd2a7dd5fa43
vikash surin <vikashsurin10@gmail.com>
2020-08-20 18:14:23 +0530

    Updates

---
+++ precache-manifest.be362c5daa48af0a2b5062c628349e39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bf8210d544e21604e1337f4ed7d82d",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "0abaf05b36c494eb11ab",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2beead998f941c437d2",
+    "url": "/test/static/js/2.ac5db91e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.ac5db91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abaf05b36c494eb11ab",
+    "url": "/test/static/js/main.65abcd3a.chunk.js"
+  },
+  {
+    "revision": "4763a8c77d152ebc2831",
+    "url": "/test/static/js/runtime-main.861cf37a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
